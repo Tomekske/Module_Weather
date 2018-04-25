9f21addb4013b3317130eabf601225db108ce26b
Tomekske <joostenstomek@gmail.com>
2018-04-25 21:38:30 +0200

    log: 6
    Updated
    - ResponsHandler
    - Import method
    - ConfigHandler function

--- ResponseHandler.py
+++ ResponseHandler.py
@@ -21,19 +21,19 @@ import httplib2
 ##
 class Response():
 	##
-    ## @brief      Constructor of the Response class
-    ## @param      url The url to the website you want to get a JSON response from    
-    ##
+	## @brief      Constructor of the Response class
+	## @param      url The url to the website you want to get a JSON response from    
+	##
 	def __init__(self, url):
 		self.status = self.status(url)
-		self.json = self.querry(url,self.status)
+		self.json = self.query(url,self.status)
 
 
 
-    ##
-    ## @brief      Function to test if given URL is valid
-    ## @param      url The URL you want to test
-    ##
+	##
+	## @brief      Function to test if given URL is valid
+	## @param      url The URL you want to test
+	##
 	def status(self,url):
 		#Test URL, if it exsist return true else return false
 		try:
@@ -45,18 +45,17 @@ class Response():
 
 
 
-    ##
-    ## @brief      request to the server
-    ## @param      url The URL you want to fetch data from
-    ## @param      status The status code which is tested in te status function
-    ## @return     JSON response or error message
-    ##  
-	def querry(self,url,status):
+	##
+	## @brief      request to the server
+	## @param      url The URL you want to fetch data from
+	## @param      status The status code which is tested in te status function
+	## @return     JSON response or error message
+	##  
+	def query(self,url,status):
 		#if URL exsist get data from the server and return it. Else return error string
 		if status == True:
 			raw = requests.get(url)
 			decodeRaw = raw.content.decode("utf-8") #get content from webpage and decode bytes,replace "" with `` and convert them to a string
 			jsonData = json.loads(decodeRaw) #convert JSON string to a JSON object
 			return jsonData
-		else:
-			return 'RAW_QUERY_ERROR'
+		return 'RAW_QUERY_ERROR'
